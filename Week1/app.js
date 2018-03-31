e9963e9a48cc1733819ee99d42d1aa2f11c29f25
Sobhi Alkhuder <35468770+Sob7i@users.noreply.github.com>
2018-03-31 14:53:45 +0200

    Update app.js

--- Week1/app.js
+++ Week1/app.js
@@ -1,8 +1,96 @@
 'use strict';
-
-
-
 {
+    const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
+
+    function main(data) {
+
+        const root = document.getElementById('root');
+        const h1 = createAndAppend('h1', root, { html: 'HYF Repositories:' });
+        const header = createAndAppend('div', root);
+        const select = createAndAppend('select', header, { placeholder: 'Select a repository' });
+        select.addEventListener("change", theListener);
+
+        data.forEach(repository => {
+            const option = createAndAppend('option', select, { value: repository.name, html: repository.name });
+        });
+        const repContainer = createAndAppend('div', root);
+        repContainer.id = 'rep-container';
+        const contContainer = createAndAppend('div', root);
+        contContainer.id = 'cont-container';
+    }
+
+    function theListener(event) {
+
+        const urlContributors = 'https://api.github.com/repos/HackYourFuture/' + event.target.value + '/contributors';
+        const urlRepositories = 'https://api.github.com/repos/HackYourFuture/' + event.target.value;
+
+        fetchJSON(urlContributors, contInfo);
+        fetchJSON(urlRepositories, RepSelect);
+    }
+
+    function RepSelect(error, repData) {
+
+        if (error !== null) {
+            console.error(error.message);
+
+        } else {
+            renderRep(repData);
+        }
+
+    }
+
+    function renderRep(repData) {
+
+        const repContainer = document.getElementById('rep-container');
+        repContainer.innerHTML = '';
+
+        const list = createAndAppend('div', repContainer, { id: 'list-container' });
+        const ul = createAndAppend('ul', list, { html: '<strong> Repository information: </strong>' });
+        const li1 = createAndAppend('li', ul, { html: 'Repository:' + " " + repData.name });
+        const li2 = createAndAppend('li', ul, { html: 'Fork:' + " " + repData.forks_count });
+        const li3 = createAndAppend('li', ul, { html: 'Updated:' + " " + repData.updated_at });
+
+    }
+
+    function contInfo(error, contData) {
+
+        if (error !== null) {
+            console.error(error.message);
+
+        } else {
+            renderCont(contData);
+        }
+
+    }
+
+    function renderCont(contData) {
+
+        const contContainer = document.getElementById('cont-container');
+        contContainer.innerHTML = '';
+
+        const ol = createAndAppend('ol', contContainer, { id: 'Contributors-style', html: '<strong>Contributors:</strong>' });
+
+        contData.forEach(contData => {
+            const li = createAndAppend('li', ol, {
+                html: " " + contData.login + " " +
+                    contData.contributions + '<br>' + "<img src=" + contData.avatar_url + ">"
+            });
+
+        });
+    }
+
+    function callback(error, data) {
+
+        if (error !== null) {
+            console.error(error.message);
+
+        } else {
+            main(data);
+
+        }
+    }
+    fetchJSON(url, callback);
+
 
     function fetchJSON(url, cb) {
 
@@ -22,124 +110,21 @@
             }
         }
         xhr.send();
-
-
     }
 
-
-    const url = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
-    // const url1 = 'https://api.github.com/repos/HackYourFuture/';
-
-    fetchJSON(url, (error, data) => {
-
-        if (error !== null) {
-
-            console.error(error);
-
-        } else {
-
-            repsSelect(data);
-
-        }
-
-    });
-
-
-
-    function repsSelect(data) {
-
-        console.log(data);
-        const firstRepo = console.log(data[0]);
-
-
-        fetchJSON(data[0].contributors_url, (error, contributors) => {
-
-            if (error !== null) {
-
-                console.log(error.message);
-
+    function createAndAppend(name, parent, options = {}) {
+        const elem = document.createElement(name);
+        parent.appendChild(elem);
+        Object.keys(options).forEach(key => {
+            const value = options[key];
+            if (key === 'html') {
+                elem.innerHTML = value;
             } else {
-
-                contributorsSelect(contributors);
-
+                elem.setAttribute(key, value);
             }
-
         });
-
-        const root = document.getElementById('root');
-
-        const select = createAndAppend('select', root);
-
-        select.addEventListener('change', () => repsSelect(select.value));
-
-        data.forEach(element => {
-
-            const option = createAndAppend('option', select);
-            option.setAttribute('value', element.url);
-            option.innerHTML = element.name;
-
-        });
-
+        return elem;
     }
 
-    function contributorsSelect(contributors) {
-
-        console.log(contributors);
-    }
-
-    function htmlBody(data) {
-        const listContainer = createAndAppend('div', root);
-        listContainer.setAttribute('class', list_container);
-
-        const table = createAndAppend('table', listContainer);
-        const table = createAndAppend('tbody', table);
-        const table = createAndAppend('tr', tbody);
-        const table = createAndAppend('td1', tr);
-        const table = createAndAppend('td2', td1);
-
-
-    }
-
-    function createAndAppend(tagName, parent) {
-
-        const element = document.createElement(tagName);
-
-        parent.appendChild(element);
-
-        return element;
-
-    }
-
-
-
+    window.onload = theListener;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
